32bfb4b9eee3f883c6a33a1b5d9997f5c76647a8
Amit Rathi <amit.rathi.k@gmail.com>
2023-07-10 21:06:50 -0400

    rewriting methods with async methods to avoid callback hell

--- index.js
+++ index.js
@@ -15,12 +15,29 @@ const file = './data/activities/Chase8717_Activity_20230710.CSV';
 // Functions
 const convertCSVToJSON = require('./functions/convertCSVToJSON');
 
-fs.readFile(path.resolve(file),'utf-8', (error, data) => {
-	if(error){
+// fs.readFile(path.resolve(file),'utf-8', (error, data) => {
+// 	if(error){
+// 		console.log(error);
+// 		return;
+// 	}
+// 	console.log(convertCSVToJSON(data).length)
+// 	// console.table(convertCSVToJSON(data));
+// });
+
+async function displayData(){
+	const data = await fs.promises.readFile(path.resolve(file),'utf-8').catch((error) => {
 		console.log(error);
-		return;
-	}
-	console.log(convertCSVToJSON(data).length)
-	// console.table(convertCSVToJSON(data));
-});
+	});
+	console.log(data);
+}
+
+displayData();
+// async function test(){
+// 	await new Promise(() =>
+//     fs.readFile(path.resolve(file),'utf-8'),
+// (error) => {
+// 	console.log(error);
+// })
+// }
+
 
