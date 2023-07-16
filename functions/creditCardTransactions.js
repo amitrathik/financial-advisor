08f218462a51549d1b183cddfe24c434a6e85ef1
Amit Rathi <amit.rathi.k@gmail.com>
2023-07-16 00:51:40 -0400

    more notes on what i might be stuck on

---
+++ functions/creditCardTransactions.js
@@ -0,0 +1,5 @@
+// updated Credit card JS with statement date to establish starting point
+// since it's difficult to determine how much was owed with the transactions alone
+// for card 5550 -> 2976 -> 7089
+// I have all transactions from 2021+
+// I recorded the balance due 02/01/2021 based on statemetn from 01/04/2021
--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const groupByYear = require('./functions/groupByYear');
 const getActivity = require('./functions/getActivity');
 const getTransactions = require('./functions/getTransactions');
 const sortTransactions = require('./functions/sortTransactions');
+
+// Objects and Models
+const creditCards = require('./data/creditcards/credit_cards');
+
 // Requiring express in our server
 const express = require('express');
 const app = express();
@@ -26,6 +30,35 @@ async function getAccountData (accountNumber = '8717'){
 		return sortTransactions('2021', convertCSVToJSON(transactions));
 	})
 }
+
+// establishing starting point with primary acct 8717
+// starts with 10/04/2021
+
+// need to establish standing point with credit card balances relative with that date
+
+// I will have to look at the statement for 5550 to determine balance as of ~10/04/2021
+// I will also check 0529
+// I don't think I got 1864 until ~11/2021 
+// for 5550(2976), I'd like to calculate the balance based on the transactions, but i'll also grab it from the statement just to double-check the math
+
+function getCard(cardNumber = '2976'){
+	// find credit card by card number
+	return creditCards.find((card) => {
+		return card.currentEndingNumber == cardNumber || card.formerEndingNumbers.includes(cardNumber);
+	})
+}
+
+const card = getCard();
+const transactions = getAccountData('2976').then((transactions) => {
+	// based on card model, calculate the balance for each statement 
+	// i can use the opening and closing date
+	// basically I want to determine what the balance is for this card around 10/2021 
+	return transactions;
+
+});
+// i have two files in activities for this card, i know there is some overlap, maybe I can merge the two records
+// for simplicity I might just focus on 2976 for now.
+
 
 // TODO find all credit card payments from primary acct = PERSONAL CHK 8717
 // Should have description something like "Payment to BANK card ending in #### MM/DD"
@@ -54,24 +87,21 @@ async function getAccountData (accountNumber = '8717'){
 
 
 // API Endpoints
-app.get('/api', async function(req, res) {
-	const data = await getAccountData();
-	res.json(data);
-});
+// s
 
-// Server-Rendered Front End
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use('/public',express.static(__dirname + '/public'));
-// use res.render to load up an ejs view file
-// index page
-app.get('/ui', async function(req, res) {
-	const data = await getAccountData();
-  	res.render('pages/index', {data: data});
+// // Server-Rendered Front End
+// // set the view engine to ejs
+// app.set('view engine', 'ejs');
+// app.use('/public',express.static(__dirname + '/public'));
+// // use res.render to load up an ejs view file
+// // index page
+// app.get('/ui', async function(req, res) {
+// 	const data = await getAccountData();
+//   	res.render('pages/index', {data: data});
   
-});
+// });
 
-// // Setting the server to listen at port 3000
-app.listen(3000, function(req, res) {
-	console.log("Server is running at port 3000");
-});
+// // // Setting the server to listen at port 3000
+// app.listen(3000, function(req, res) {
+// 	console.log("Server is running at port 3000");
+// });
