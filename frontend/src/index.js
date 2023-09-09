6af45db6cb3ea04cc1d53de0b9e40358df720d39
Amit Rathi <amit.rathi.k@gmail.com>
2023-09-08 20:46:54 -0400

    added indexdb web storage functionality to focus entirely on client side demo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/main.scss";
 
-// Views
-import { AccountsList } from "./views/Accounts/AccountsList";
-import { AccountDetails } from "./views/Accounts/AccountDetails";
-
 // Services
 import { getAccounts } from "./lib/accountsService";
 import { getTransactions } from './lib/transactionsService';
 import { getCards } from "./lib/cardsService";
 
+// IndexDB 
+const db = window.indexedDB.open("fa_db", 1);
+// these two event handlers act on the IDBDatabase object,
+// when the database is opened successfully, or not
+db.onerror = (event) => {
+	console.log("error loading db")
+}
+
+db.onsuccess = (event) => {
+	console.log("db initialized")
+}
+
+db.onupgradeneeded =(event) => {
+	const db = event.target.result;
+
+	db.onerror = (event) => {
+	  console.log("error loading db")
+	};
+
+	const objectStore = db.createObjectStore("accounts", {
+		keyPath: "number",
+	});
+	objectStore.createIndex("type", "type", { unique: false });
+	objectStore.createIndex("balance", "balance", { unique: false });
+	objectStore.createIndex("created_at", "created_at", { unique: false });
+	objectStore.createIndex("updated_at", "updated_at", { unique: false });
+}
+
 class App extends React.Component {
     constructor(props) {
+		// lets get a DB setup in the browser for testing / local storage / demo prep
 		super(props);
         this.state = {
+			route : '',
 			accounts: [],
 			cards : [],
 			selectedAccount:'',
@@ -26,13 +52,13 @@ class App extends React.Component {
 
 	
 	componentDidMount(){
-		Promise.all([getAccounts(), getCards()])
-		 .then(([accounts, cards]) => {
-		   this.setState({
-				accounts : accounts, 
-				cards : cards
-			})
-		 });
+		// Promise.all([getAccounts(), getCards()])
+		//  .then(([accounts, cards]) => {
+		//    this.setState({
+		// 		accounts : accounts, 
+		// 		cards : cards
+		// 	})
+		//  });
 	}
 
 
@@ -67,22 +93,31 @@ class App extends React.Component {
 	}
 
     render() {
-		const View = this.state.selectedAccount ? 
-						<AccountDetails 
-							account={this.state.selectedAccount}
-							transactions={this.state.transactions}
-							cards={this.state.cards}
-							resetAccountSelection={this.resetAccountSelection}
-							handleYearSelection={this.handleYearSelection}
-							handleMonthSelection={this.handleMonthSelection}
-							selectedYear={this.state.selectedYear}
-							selectedMonth={this.state.selectedMonth}
-						/> : 
-						<AccountsList 
-							items={this.state.accounts}
-							cards={this.state.cards}
-							handleAccountSelection={this.handleAccountSelection}
-						/> 
+		const view = '';
+		const route = this.state.route;
+		switch(route) {
+			case 'accounts':
+				view = this.state.selectedAccount ? 
+					<AccountDetails 
+						account={this.state.selectedAccount}
+						transactions={this.state.transactions}
+						cards={this.state.cards}
+						resetAccountSelection={this.resetAccountSelection}
+						handleYearSelection={this.handleYearSelection}
+						handleMonthSelection={this.handleMonthSelection}
+						selectedYear={this.state.selectedYear}
+						selectedMonth={this.state.selectedMonth}
+					/> : 
+					<AccountsList 
+						items={this.state.accounts}
+						cards={this.state.cards}
+						handleAccountSelection={this.handleAccountSelection}
+					/> 
+				break;
+			default:
+
+				break;
+		}
         return (
             <div>
 				<div className="App">
@@ -91,7 +126,7 @@ class App extends React.Component {
 					</div>
 					
 					<div className="View">
-						{View}
+						{view}
 					</div>
 				</div>
             </div>
